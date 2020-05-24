b7c7e6e572ab0ce8d0c260449ca4b2a3da77c8a1
Raiu1210 <baio1484@gmail.com>
2020-05-24 23:19:24 +0900

    fixed some ui

--- src/views/TopView.vue
+++ src/views/TopView.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <center>
+      <br>
+      <img :src="this.$store.state.user.photoURL">
+      <h3>{{this.$store.state.user.displayName}}</h3>
+      <!-- <h3>uid : {{this.$store.state.user.uid}}</h3> -->
+
       <div class="ejaculate_button">
         <v-btn @click="save_evaculate" height="200" width="90%" color="pink lighten-1" dark>
           <span>射精した！</span>  
         </v-btn>
       </div>
-
-      <br>
-      <img :src="this.$store.state.user.photoURL">
-      <h3>displayName : {{this.$store.state.user.displayName}}</h3>
-      <h3>uid : {{this.$store.state.user.uid}}</h3>
     </center>
   </div>  
 </template>
