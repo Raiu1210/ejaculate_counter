237f82b3a5f496996ac4cd6f8a639232a67ad1f5
Raiu1210 <baio1484@gmail.com>
2020-05-24 23:14:35 +0900

    stats top card

--- src/views/Stats.vue
+++ src/views/Stats.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
     <center>
-      <h1>Hello</h1>
+      <v-card class="top-card" width="60%">
+        <v-card-title class="headline">あなたの累計射精回数</v-card-title>
+        <v-divider class="mx-3"></v-divider>
+        <v-card-text>
+          <div class="body-1 mb-1">{{ejaculated_timstamps.length}}回</div>
+        </v-card-text>
+      </v-card>
+      <span>{{ejaculated_timstamps}}</span>
+      <Chart></Chart>
     </center>
   </div>  
 </template>
@@ -9,13 +17,14 @@
 
 <script>
 import firebase from 'firebase'
+import { Bar } from 'vue-chartjs'
+import Chart from '@/components/Chart.vue';
 
 export default {
+  extends: Bar,
   data() {
     return {
-      db: null,
-      user: null,
-      allData: []
+      ejaculated_timstamps: [],
     }
   },
   methods: {
@@ -27,21 +36,32 @@ export default {
         this.$store.state.user = user
         this.$store.state.login = true
 
-        // this.db = firebase.firestore();
-        // const collectionRef = this.db.collection(this.$store.state.user.uid)
-
-        console.log(this.$store.state.user.uid)
-        
+        var tmp_ejaculated_timstamps = []  
         firebase.firestore().collection(this.$store.state.user.uid).get().then(function(querySnapshot) {
           querySnapshot.forEach(function(doc) {
             // doc.data() is never undefined for query doc snapshots
+            tmp_ejaculated_timstamps.push(doc.id)
             console.log(doc.id, " => ", doc.data());
           });
         });
                       
-
+        this.ejaculated_timstamps = tmp_ejaculated_timstamps
+        console.log(this.ejaculated_timstamps)
       }
     });
-  }
+  },
+  mounted () {
+    this.renderChart(this.data, this.options)
+  },
+  components: {
+    Chart,
+  },
 }
 </script>
+
+
+<style scoped>
+  .top-card {
+    margin: 100px 0px 0px 0px;
+  }
+</style>
