09b6e650e2ad5fdec6d13e909d295e4a2d1a684f
Raiu1210 <baio1484@gmail.com>
2020-06-07 22:54:42 +0900

    fixed algorithm

--- src/views/TopView.vue
+++ src/views/TopView.vue
@@ -8,19 +8,16 @@
 
       <div class="ejaculate_button">
         <v-btn @click="save_evaculate" height="200" width="90%" color="pink lighten-1" dark>
-          <span>射精した！</span>  
+          <span class="ejaculated">射精した！</span>  
         </v-btn>
       </div>
 
-      <ShareNetwork
-          class="share_button"
-          network="twitter"
-          url="https://ejaculate-counter.firebaseapp.com/#/"
-          v-bind:title="tweet_msg"
-          hashtags="わたしの射精記録,射精記録,射精,ザーメン"
-        >
-        <span class="guide_label">Twitterでシェア</span>
-      </ShareNetwork>
+
+      <div class="ejaculate_button">
+        <v-btn class="share_button" @click="tweet_my_stats" height="70" width="60%" color="blue lighten-1" dark>
+          <span class="share_label">Twitterでシェア</span>  
+        </v-btn>
+      </div>
     </center>
   </div>  
 </template>
@@ -33,7 +30,6 @@ export default {
   data() {
     return {
       tweet_msg: 'aaa',
-      ejaculated_timstamps: []
     }
   },
   methods: {
@@ -66,11 +62,43 @@ export default {
       var sec   = ( d.getSeconds() < 10 ) ? '0' + d.getSeconds() : d.getSeconds();
   
       return year + '-' + month + '-' + day + '-' + hour + '-' + min + '-' + sec
+    },
+    async tweet_my_stats() {
+      await firebase.firestore().collection(this.$store.state.user.uid).get().then(function(querySnapshot) {
+        var ejaculated_timstamps = []
+        querySnapshot.forEach(function(doc) {
+          ejaculated_timstamps.push(doc.id)
+        });
+
+        const time_zone = ['0時~2時', '2時~4時', '4時~6時', '6時~8時', '8時~10時', '10時~12時', '12時~14時', '14時~16時', '16時~18時', '18時~20時', '20時~22時', '20時~24時']
+        const num_data = ejaculated_timstamps.length
+        var time_data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ejaculated_timstamps.forEach(element => {
+          const hour = element.slice(11, 13)
+          const index = Math.floor(hour/2)
+          time_data[index] += 1 / num_data
+        });
+        const max_index = time_data.indexOf(Math.max.apply(null, time_data))
+
+        var percent_data = []
+        time_data.forEach(element => {
+          percent_data.push((Math.round(element * 100)).toString() + "%25")
+        })
+
+        var shareURL = "https://twitter.com/intent/tweet?text=" + 
+                       "私の射精記録%0a%0a" +
+                       "【射精回数】%0a     " + ejaculated_timstamps.length + "回%0a%0a" +
+                       "【推計射精量】%0a     約" + ejaculated_timstamps.length*3.5 + " [ml]%0a%0a" +
+                       "【メインの射精時間帯】%0a     " + time_zone[max_index] + "  (" + percent_data[max_index] +  ")%0a%0a" +
+                       "%20%23射精 %20%23私の射精記録 %20%23ザーメン %0a%0a%0a" +
+                       "&url=https://ejaculate-counter.firebaseapp.com/#/  %0a%0a%0a"
+        location.href = shareURL
+      });
+
+      
     }
   },
   async created() {
-    var self = this
-    
     await firebase.auth().onAuthStateChanged(user => {
       if(user) {
         this.$store.state.user = user
@@ -84,31 +112,23 @@ export default {
       } else {
         this.$store.state.login = false
       }
-    })
-
-    // get ejaculate timestamps
-    await firebase.firestore().collection(this.$store.state.user.uid).get().then(function(querySnapshot) {
-      querySnapshot.forEach(function(doc) {
-        self.ejaculated_timstamps.push(doc.id)
-      });
-    });
-
-    this.tweet_msg = "私の射精記録\n\n" + 
-                     "【射精回数】\n\t" + this.ejaculated_timstamps.length + "回\n\n" +
-                     "【推計射精量】\n\t" + this.ejaculated_timstamps.length*3.5 + " [ml]\n\n"
-                    
+    })  
   }
 }
 </script>
 
 
 <style scoped>
-  span {
+  .ejaculated {
     font-size: 50px;
   }
 
   .ejaculate_button {
     margin: 30px;
+  }
+
+  .share_label {
+    font-size: 20px;
   }
 
   .share_button {
